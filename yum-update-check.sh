85ae9018468940409e1ab5df9fca7470923e36e0
lucifersgit <42358956+lucifersgit@users.noreply.github.com>
2019-09-26 17:05:28 +0100

    Create yum-update-check.sh

---
+++ yum-update-check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $UID -ne 0 ]]; then
+   echo "This script must be run as root or with 'sudo'" 
+   exit 1
+fi
+
+echo "Clearing yum cache"
+yum clean all
+
+cmd="/usr/bin/yum check-update"
+status=$?
+update="/usr/bin/yum update"
+
+$cmd
+status=$?
+
+if [ $status -eq 100 ]
+then 
+	echo "Updates are available. Running 'yum update'"
+	$update
+	exit 0	
+else
+	echo "No system updates are available"
+	exit 1
